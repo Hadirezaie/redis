2540441faea371a67005eb0db7e405e443806ef3
HadiRezaie <hadi.rezaie2015@gmail.com>
2025-05-12 09:16:46 +0430

    updated.

--- src/main/java/com/newcoders/redis/redis/RedisApplication.java
+++ src/main/java/com/newcoders/redis/redis/RedisApplication.java
@@ -2,19 +2,15 @@ package com.newcoders.redis.redis;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-// @SpringBootApplication
 public class RedisApplication {
 
 	private static final Log LOG = LogFactory.getLog(RedisApplication.class);
 
 	public static void main(String[] args) {
-		// SpringApplication.run(RedisApplication.class, args);
 
 		LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory();
 		connectionFactory.afterPropertiesSet();
